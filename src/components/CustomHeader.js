d0d20902b26260e191afbec1bf1265cd454b754c
Glansa-Solutions <glansa.solutions777@gmail.com>
2025-07-09 18:06:46 +0530

    removed custom header border bottom

--- src/components/CustomHeader.js
+++ src/components/CustomHeader.js
@@ -42,8 +42,8 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     paddingBottom: 0,
     backgroundColor: "#fff",
-    borderBottomColor: "#eee",
-    borderBottomWidth: 1,
+    // borderBottomColor: "#eee",
+    // borderBottomWidth: 1,
   },
   topRow: {
     flexDirection: "row",
